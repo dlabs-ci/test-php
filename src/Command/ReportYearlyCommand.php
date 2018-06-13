4e8b0e4fc3f6ae6611796529195d3b87b9c0c59a
ninadvc <n.jovicic@dvcsolutions.com>
2018-06-13 10:12:25 +0200

    implemented model and query for tasks

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -6,6 +6,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
+
+use BOF\Models\Profile;
 
 class ReportYearlyCommand extends ContainerAwareCommand
 {
@@ -14,19 +17,25 @@ class ReportYearlyCommand extends ContainerAwareCommand
         $this
             ->setName('report:profiles:yearly')
             ->setDescription('Page views report')
+            ->addArgument('year', InputArgument::OPTIONAL)
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+     
         /** @var $db Connection */
-        $io = new SymfonyStyle($input,$output);
+        $io = new SymfonyStyle($input, $output);
         $db = $this->getContainer()->get('database_connection');
-
-        $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
+        $profile = new Profile($db);
+        if ($input->hasArgument('year') && $input->getArgument('year') != NULL) {
+            $year = $input->getArgument('year');
+        } else {
+            $year = '2008';
+        }
+        $profiles = $profile->firstQuery($year)->fetchAll();
 
         // Show data in a table - headers, data
-        $io->table(['Profile'], $profiles);
-
+        $io->table(['Profile', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], $profiles);
     }
 }
--- src/Models/Profile.php
+++ src/Models/Profile.php
@@ -1,10 +1,54 @@
 <?php
-use Doctrine\ORM\Mapping as ORM;
+
+namespace BOF\Models;
+
 /**
  * Description of Profile
  *
  * @author Nikolina Jovicic
  */
 class Profile {
-    //put your code here
+
+    private $db;
+    private $profile_id;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function getProfileId() {
+
+        $profiles = $this->db->query('SELECT profile_name FROM profiles');
+        return $profiles;
+    }
+    
+    public function findByName($name){
+        return $this->db->query("SELECT profile_name FROM profiles where profile_name='$name'");
+    }
+    
+    public function firstQuery($year){
+        
+        return $this->db->query("SELECT  profile_name, 
+if(sum(CASE WHEN MONTH(date)=1 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=1 THEN views ELSE 0 END )) as 'JAN',
+if(sum(CASE WHEN MONTH(date)=2 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=2 THEN views ELSE 0 END )) as 'FEB',
+if(sum(CASE WHEN MONTH(date)=3 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=3 THEN views ELSE 0 END )) as 'MAR',
+if(sum(CASE WHEN MONTH(date)=4 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=4 THEN views ELSE 0 END )) as 'APR',
+if(sum(CASE WHEN MONTH(date)=5 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=5 THEN views ELSE 0 END )) as 'MAY',
+if(sum(CASE WHEN MONTH(date)=6 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=6 THEN views ELSE 0 END )) as 'JUN',
+if(sum(CASE WHEN MONTH(date)=7 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=7 THEN views ELSE 0 END )) as 'JUL', 
+if(sum(CASE WHEN MONTH(date)=8 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=8 THEN views ELSE 0 END )) as 'AUG',        
+if(sum(CASE WHEN MONTH(date)=9 THEN views ELSE 0 END )=0,'n/a',  sum(CASE WHEN MONTH(date)=9 THEN views ELSE 0 END )) as 'SEP',  
+if(sum(CASE WHEN MONTH(date)=10 THEN views ELSE 0 END )=0,'n/a', sum(CASE WHEN MONTH(date)=10 THEN views ELSE 0 END )) as 'OCT',
+if(sum(CASE WHEN MONTH(date)=11 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=11 THEN views ELSE 0 END )) as 'NOV',
+if(sum(CASE WHEN MONTH(date)=12 THEN views ELSE 0 END )=0, 'n/a', sum(CASE WHEN MONTH(date)=12 THEN views ELSE 0 END )) as 'DEC' 
+FROM bof_test.views  LEFT JOIN  bof_test.profiles ON views.profile_id=profiles.profile_id
+WHERE YEAR(date) = $year
+GROUP BY bof_test.profiles.profile_id
+ORDER BY profile_name");
+    }
+
+    private $profile_name;
+    
+    
+
 }
