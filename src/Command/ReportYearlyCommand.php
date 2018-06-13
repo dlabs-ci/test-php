f6ff162c516a87707278e1db44513bdce39bd46e
gun7 <s.zupljanin@dvcsolutions.com>
2018-06-13 09:00:43 +0200

    inserted the generated info into symfony IO table

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -26,29 +26,23 @@ class ReportYearlyCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /** @var $db Connection */
-        // $io = new SymfonyStyle($input,$output);
+        $io = new SymfonyStyle($input,$output);
         // $db = $this->getContainer()->get('database_connection');
 
         // $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
 
-        // // Show data in a table - headers, data
-        // $io->table(['Profile'], $profiles);
-
+        
         $em = $this->getContainer()->get('entity_manager');
         // $profile = $em->find(Profile::class, 1);
         $profile = $em->getRepository(Profile::class);
+        $profiles = $profile->findWithViewsForYear(2014);
+        // var_dump($profiles);exit;
 
-        $profiles = $profile->findBy(['profile_id' => 1]);
-        var_dump($profiles[0]->getViews()[6]->getViews());
+        // // Show data in a table - headers, data
+        $io->table(['Profile'], $profiles);
 
-        // $sql = "SELECT views.profile_id, profile_name, 
-        // (CASE WHEN (sum(CASE WHEN MONTH(`views`.`date`)=1 THEN views ELSE 0 END )) = 0 THEN 'n/a' ELSE sum(CASE WHEN MONTH(`views`.`date`)=1 THEN views ELSE 0 END ) END) as 'JAN',  
-        // (CASE WHEN (sum(CASE WHEN MONTH(`views`.`date`)=9 THEN views ELSE 0 END )) = 0 THEN 'n/a' ELSE sum(CASE WHEN MONTH(`views`.`date`)=9 THEN views ELSE 0 END ) END) as 'SEP',
-        // (CASE WHEN (sum(CASE WHEN MONTH(`views`.`date`)=10 THEN views ELSE 0 END )) = 0 THEN 'n/a' ELSE sum(CASE WHEN MONTH(`views`.`date`)=10 THEN views ELSE 0 END ) END) as 'OCT'
-        // FROM bof_test.views  LEFT JOIN  bof_test.profiles ON views.profile_id=profiles.profile_id
-        // WHERE YEAR(date) = 2014
-        // GROUP BY profile_id, profile_name
-        // ORDER BY profile_name";
+        // $profiles = $profile->findBy(['profile_id' => 1]);
+        // var_dump($profiles[0]->getViews()[6]->getViews());
 
         // $stmt = $em->getConnection()->prepare($sql);
         // $stmt->execute();
--- src/Repository/ProfileRepository.php
+++ src/Repository/ProfileRepository.php
@@ -3,8 +3,74 @@
 namespace BOF\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Select;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 class ProfileRepository extends EntityRepository
 {
-    
+    /**
+     * Array of months
+     *
+     * @var array
+     */
+    public $months = [
+        1 => 'Jan', 2 => 'Feb', 3 => 'Mar', 4 => 'Apr', 5 => 'May', 6 => 'Jun', 7 => 'Jul', 8 => 'Aug', 9 => 'Sep', 10 => 'Oct', 11 => 'Nov', 12 => 'Dec'
+    ];
+
+    /**
+     * Get sum of profile views by months for given year
+     *
+     * @param integer $year The year for querying the profile views
+     * @return array
+     */
+    public function findWithViewsForYear($year)
+    {
+        $sql = $this->buildQueryForYearViews($year);
+        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        $stmt->execute([
+            'year' => $year
+        ]);
+
+        return $stmt->fetchAll();
+    }
+
+    /**
+     * Generate query string for executing
+     *
+     * @param integer $year
+     * @return string
+     */
+    protected function buildQueryForYearViews($year)
+    {
+        $sql = "SELECT profile_name, ";
+        $sql .= $this->generateSelectsForYearViews();
+        
+        $sql .= "FROM bof_test.views  LEFT JOIN  bof_test.profiles ON views.profile_id = profiles.profile_id
+        WHERE YEAR(date) = :year
+        GROUP BY views.profile_id
+        ORDER BY profiles.profile_name";
+
+        return $sql;
+    }
+
+    /**
+     * Generate select clauses for the yearly profile views
+     *
+     * @return string
+     */
+    protected function generateSelectsForYearViews()
+    {
+        $sql = '';
+        foreach($this->months as $index => $month) {
+            //Build CASE select clause for getting the needed values per month
+            $queryPart = "sum(CASE WHEN MONTH(`views`.`date`) = {$index} THEN views ELSE 0 END )";
+            $sql .= "(CASE WHEN ({$queryPart}) = 0 THEN 'n/a' ELSE {$queryPart} END) as '{$month}' ";
+
+            if($index != count($this->months)) { //If the element is last in array, don't add comma
+                $sql .= ', ';
+            }
+        }
+
+        return $sql;
+    }
 }
