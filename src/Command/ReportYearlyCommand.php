0bde3ab5217008121a8ef81823362fc0d42cbf7d
Igor Šinkovec <igor.sinkovec@gmail.com>
2018-11-10 19:21:09 +0100

    Display report in a table

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace BOF\Command;
 
 use Doctrine\DBAL\Driver\Connection;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -41,8 +43,57 @@ class ReportYearlyCommand extends ContainerAwareCommand
         $io = new SymfonyStyle($input,$output);
         $db = $this->getContainer()->get('database_connection');
 
+        $year = $input->getArgument(self::ARG_YEAR);
 
+        // set default value for the optional argument
+        if (is_null($year)) {
+            $year = date('Y');
+        }
 
+        // sql select statement
+        $sql =
+           'SELECT
+                p.profile_id,
+                p.profile_name,
+                MONTH(v.`date`) AS month_num,
+                SUM(v.views) AS sum_views
+            FROM profiles p
+            LEFT OUTER JOIN views v ON p.profile_id = v.profile_id
+            WHERE YEAR(v.`date`) = :year
+            GROUP BY p.profile_id, month_num
+            ORDER BY p.profile_name, month_num'
+        ;
+
+        /** @var PDOStatement */
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam('year', $year);
+        $stmt->execute();
+
+        /** @var array */
+        $queryResponse = $stmt->fetchAll();
+
+        $result = [ ];
+
+        // map resultset in a single pass
+        foreach ($queryResponse as $responseRow) {
+            $profileName = $responseRow['profile_name'];
+            $monthNum = $responseRow['month_num'];
+
+            $result[$profileName][0] = $profileName;
+            $result[$profileName][$monthNum] = $responseRow['sum_views'];
+        }
+
+        $tableHeader = [
+            'Profile', // @TODO: display year
+            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+        ];
+
+        $table = new Table($output);
+        $table
+            ->setHeaders($tableHeader)
+            ->setRows($result)
+            ->render()
+        ;
     }
 
     private function validateYear($year)
