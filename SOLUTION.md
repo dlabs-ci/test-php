2015fbb25cdaf0844c2629e2398d367b2b42ce31
Luka Leskovsek <luka@leskovsek.si>
2018-11-26 12:30:57 +0100

    Extra explanations. Wrong SELECT statement ... damn damn damn

--- SOLUTION.md
+++ SOLUTION.md
@@ -15,7 +15,7 @@ But I'm a quick learner and I always try to follow good coding practices (the ge
 
 In my opinion, a more suitable solution would be to build a RESTFull API for fetching report data. The service would accept input arguments and returned a structured JSON response object. But given that this is not a production code and you don't need to spend time on the presentational part of the app (view) it is a good fit.
 
-If I'm not wrong - the idea here is that you have a single command report class that is responsible for all Yearly reports (Views, Orders, Sales, ...). It would be more suitable that there would be a command-object-per-report  (eg. for yearly views per profile). Each command class could use different query objects througout  it's use - byYear , byYearAndProfile, ... . This way the code is much more structured in future proofed.
+If I'm not wrong - the idea here is that you have a single command report class that is responsible for all Yearly reports (Views, Orders, Sales, ...). It would be more suitable that there would be a command-object-per-report  (eg. for yearly views per profile). Each command class could use different query objects througout  it's use - byYear , byYearAndProfile, ... . This way the code is much more structured in future proofed. At this stage there is also no user authentication so each individual that has access to execute this command can view the results.
 
 
 Test cases : 
--- src/Reports/ReportyViewCountByProfile.php
+++ src/Reports/ReportyViewCountByProfile.php
@@ -10,19 +10,19 @@ class ReportyViewCountByProfile
 
     public function getByYear($arg_year)
     {
-        $profiles = ("SELECT pr.profile_name, count(vi.views) sum ,
-                        COUNT(CASE WHEN MONTH(vi.date) = '1' THEN  (vi.views) END) as Jan,
-                        COUNT(CASE WHEN MONTH(vi.date) = '2' THEN  (vi.views) END) as 'Feb',
-                        COUNT(CASE WHEN MONTH(vi.date) = '3' THEN  (vi.views) END) as 'Mar',
-                        COUNT(CASE WHEN MONTH(vi.date) = '4' THEN  (vi.views) END) as 'Apr',
-                        COUNT(CASE WHEN MONTH(vi.date) = '5' THEN  (vi.views) END) as 'May',
-                        COUNT(CASE WHEN MONTH(vi.date) = '6' THEN  (vi.views) END) as 'Jun',
-                        COUNT(CASE WHEN MONTH(vi.date) = '7' THEN  (vi.views) END) as 'Jul',
-                        COUNT(CASE WHEN MONTH(vi.date) = '8' THEN  (vi.views) END) as 'Avg',
-                        COUNT(CASE WHEN MONTH(vi.date) = '9' THEN  (vi.views) END) as 'Sep',
-                        COUNT(CASE WHEN MONTH(vi.date) = '10' THEN  (vi.views) END) as 'Oct',
-                        COUNT(CASE WHEN MONTH(vi.date) = '11' THEN  (vi.views) END) as 'Nov',
-                        COUNT(CASE WHEN MONTH(vi.date) = '12' THEN  (vi.views) END) as 'Dec'
+        $profiles = ("SELECT pr.profile_name, FORMAT(SUM(vi.views),0) sum ,
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '1' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '2' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '3' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '4' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '5' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '6' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '7' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '8' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '9' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '10' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '11' THEN  (vi.views) END), 0),
+                        FORMAT(SUM(CASE WHEN MONTH(vi.date) = '12' THEN  (vi.views) END), 0)
                     FROM profiles pr
                         LEFT JOIN views vi on pr.profile_id = vi.profile_id
                     WHERE year(vi.date) = $arg_year
