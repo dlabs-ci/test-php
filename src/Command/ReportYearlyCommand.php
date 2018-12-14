2bf0552154bbc6d58b539eaf356dff10fa0440f7
Robert Smalc <robert@smalc.si>
2018-12-14 14:54:41 +0100

    Task done

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -6,6 +6,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Tightenco\Collect\Support\Collection;
+use Symfony\Component\Console\Input\InputArgument;
 
 class ReportYearlyCommand extends ContainerAwareCommand
 {
@@ -13,6 +15,7 @@ class ReportYearlyCommand extends ContainerAwareCommand
     {
         $this
             ->setName('report:profiles:yearly')
+            ->addArgument('year', InputArgument::REQUIRED, 'For which year do you need a report?')
             ->setDescription('Page views report')
         ;
     }
@@ -23,10 +26,59 @@ class ReportYearlyCommand extends ContainerAwareCommand
         $io = new SymfonyStyle($input,$output);
         $db = $this->getContainer()->get('database_connection');
 
-        $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
+        if($input->getArgument('year')){
+            $yearOfDisplay = $input->getArgument('year');
+        }
 
-        // Show data in a table - headers, data
-        $io->table(['Profile'], $profiles);
+        $profiles = collect($db->query('SELECT profile_name, date, views FROM profiles as p LEFT JOIN views as v ON p.profile_id=v.profile_id WHERE date LIKE "'.$yearOfDisplay.'-%" ORDER BY p.profile_name, v.date')->fetchAll());
+
+        // Group by profiles and order by alphabetic order
+        $export = $profiles->groupBy('profile_name')->sortBy('profile_name');
+
+        // SUM for each user theirs data
+        $export = $export->transform(function($personData){
+
+            return $personData->groupBy(function($month){
+
+                    return date('M', strtotime($month['date']));
+
+                })->transform(function($monthValue){
+
+                    return number_format($monthValue->sum('views'));
+                });
+        });
+
+        // Display no data found.
+        if(sizeof($export->toArray()) == 0){
+           return $io->table(
+                ['Profiles - '.$yearOfDisplay],
+                [
+                    ['n/a']
+                ]
+            );
+        }
+
+        // Design array for Symfony console display
+        // Header with name of profiles and months
+        $header = $profiles->sortBy('date')->groupBy(function($val) {
+
+            return date('M', strtotime($val['date']));
+
+        })->keys()->toArray();
+        array_unshift($header, 'Profiles - '.$yearOfDisplay);
+
+
+        // Add profile name in the same row as totals
+        $data = [];
+        foreach($export->toArray() as $name => $monthData){
+            $data[$name] = $monthData;
+            array_unshift($data[$name], $name);
+        }
+
+        return $io->table(
+            $header,
+            $data
+        );
 
     }
 }
---
+++ src/Tests/YearlyReport.feature
@@ -0,0 +1,4 @@
+My weakness are tests, because I haven't need to use it on small project, but on big project where I have been worked on we didn't use test. So on this project I will need to improve myself. 
+
+First you need set year for which you would like to get data. Then you need to check if you get any data. When console display the table you need to check id if return success message or exceptions.
+  Sometimes you can also checked type of given data.
