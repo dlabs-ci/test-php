8eacf0bc25d027e972d77394389d07a1af934c1b
Daniel Popov <lalabuy9948@gmail.com>
2019-01-15 12:27:45 +0100

    Done

--- SOLUTION.md
+++ SOLUTION.md
@@ -3,11 +3,49 @@ SOLUTION
 
 Estimation
 ----------
-Estimated: n hours
+Estimated: 4-5 hours
 
-Spent: x hours
-
+Spent: 3 hours
 
 Solution
 --------
 Comments on your solution
+
+So, I finished somehow, but I have to tell that I'm not that fluent in PHP and Symfony. I've been working for 3+ years with Go, Python and Node. Task in PHP was a small challenge for me.
+I guess that in a 'proper' way I would use DI from Sympfony classes to write good code.
+But I did this task mostly in plain SQL, coz It's what I remember from University, if you don't know some frameworks or language features - use plain SQL. I found most of my needs on w3schools.com.
+
+After 1h reading of documentation I found out how to add argument to this program and how to arrange the table. After that I started putting queries to Sequel Pro, this is just a tool for managing the databases. Coded till the firsts result in 1h hour, after I spent extra 30mins to fix some bugs and add `try{}catch{}`.
+
+I had an experience with some analytics in a past, I used Python3, SQL and pandas. In my opinion when you work with small data you can you some dataframe frameworks to speed up your development time. If you work with extremely large data - SQL. For better scalability or 'pair programming' I would split it in micro-services. If you are using some kind of ORM it's better to use it in REST api way, some server with fast JSON responses.
+
+Test cases: 
+
+Wrong year
+```sh
+bin/console report:profiles:yearly 201
+
+Invalid YEAR. Please enter valid one.
+```
+
+Good case
+```sh
+ bin/console report:profiles:yearly 2016
+ 
+ Everything as expected
+ ```
+ 
+ Data is missing
+ ```sh
+ bin/console report:profiles:yearly 2014
+ 
+ Everything as expected, if data is not available you will see empty boxes.
+ ```
+ 
+ Features:
+ 
+1. Definitely I would add visualization 
+2. I would make this app in rest api way with a client (Vue, React, etc)
+3. I would try to implement showing sum for custom period as on Google analytics.
+
+![screen-shot](screen.png)
--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -7,6 +7,9 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
 class ReportYearlyCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -14,19 +17,65 @@ class ReportYearlyCommand extends ContainerAwareCommand
         $this
             ->setName('report:profiles:yearly')
             ->setDescription('Page views report')
+            // Year as an argument
+            ->addArgument('year', InputArgument::REQUIRED, 'Enter the year: ')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // Getting year from argument
+        $year = $input->getArgument('year');
+        // Checking if its valid
+        if(strlen($year) != 4) {
+            $output->write("Invalid YEAR. Please enter valid one.\n");
+            exit;
+        }
+
         /** @var $db Connection */
         $io = new SymfonyStyle($input,$output);
         $db = $this->getContainer()->get('database_connection');
 
-        $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
+        // For ease of editing i would split sql query in query builder or smth like that,
+        // but I'm not that good with symfony and doctrine.
+        //
+        // One more thing, for use of GROUP BY you have to execute this query:
+        // SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+        try {
+            $data = ("SELECT profiles.profile_name,
+                    SUM(views.views) sum,
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '1' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '2' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '3' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '4' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '5' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '6' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '7' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '8' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) =  '9' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) = '10' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) = '11' THEN (views.views) END), 'N'),
+                    FORMAT(SUM(CASE WHEN MONTH(views.date) = '12' THEN (views.views) END), 'N')
+                    FROM profiles
+                        LEFT JOIN views ON profiles.profile_id = views.profile_id
+                    WHERE year(views.date) = $year
+                    GROUP BY views.profile_id, year(views.date)");
+        } catch (\Exception $ex) {
+            $output->write("Query faild: " . $ex->getMessage());
+        };
 
-        // Show data in a table - headers, data
-        $io->table(['Profile'], $profiles);
+        // Query for order, maybe later you would need to sort it in other way
+        $order = 'ORDER BY profiles.profile_name ASC';
+        $profiles = $db->query($data . $order)->fetchAll();
 
+        try {
+            // Setting up the headers and print data.
+            $io->table(['Profile ' . $year, 'Total',
+            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Avg', 'Sep', 'Oct', 'Nov', 'Dec'], $profiles);
+        } catch (\Exception $ex) {
+            $output->write("Failed to output: " . $ex->getMessage());
+        };
     }
+
 }
