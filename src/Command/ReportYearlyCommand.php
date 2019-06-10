e7372fbcaa8bac19b1d3ca870eeaca70b818f344
dlabs dlabs <dlabs@dlabss-MacBook-Pro.local>
2019-06-10 11:09:13 +0200

    Gathering and sorting data for console table view.

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -19,14 +19,37 @@ class ReportYearlyCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $year = '2014';
+        $tableHeads = array(
+            0   => "Profile"
+        );
+        for ($month = 1; $month <= 12 ;$month++) {
+            $tableHeads[$month] = date("M", mktime(0, 0, 0, $month, 10));
+        }
+
         /** @var $db Connection */
         $io = new SymfonyStyle($input,$output);
         $db = $this->getContainer()->get('database_connection');
 
-        $profiles = $db->query('SELECT profile_name FROM profiles')->fetchAll();
+        $rawData = $db->query('
+            SELECT profiles.profile_id, profiles.profile_name, month(views.date) as month, sum(views.views) as sum_views
+            FROM views
+            JOIN profiles ON profiles.profile_id = views.profile_id
+            WHERE year(views.date) = 2015
+            GROUP BY month(views.date), views.profile_id
+            ORDER BY views.profile_id, month(views.date)
+        ')->fetchAll();
+
+        $tableData = array();
+        foreach ($rawData as $row) {
+            if (!isset($tableData[$row['profile_id']])) {
+                $tableData[$row['profile_id']]['Profile'] = $row['profile_name'];
+            }
+            $tableData[$row['profile_id']][$row['month']] = number_format($row['sum_views'], 0, ".", ",");
+        }
 
         // Show data in a table - headers, data
-        $io->table(['Profile'], $profiles);
+        $io->table($tableHeads, $tableData);
 
     }
 }
