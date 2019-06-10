422102b8a0240718c3ce88cd93e40dd973d4bb39
dlabs dlabs <dlabs@dlabss-MacBook-Pro.local>
2019-06-10 11:50:04 +0200

    changed data view in table according requirements.

--- src/Command/ReportYearlyCommand.php
+++ src/Command/ReportYearlyCommand.php
@@ -3,6 +3,7 @@ namespace BOF\Command;
 
 use Doctrine\DBAL\Driver\Connection;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -14,38 +15,64 @@ class ReportYearlyCommand extends ContainerAwareCommand
         $this
             ->setName('report:profiles:yearly')
             ->setDescription('Page views report')
+            ->addArgument('year', InputArgument::REQUIRED, "Show data for year...")
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $year = '2014';
-        $tableHeads = array(
-            0   => "Profile"
-        );
+        /* $year argument is required */
+        $year = $input->getArgument('year');
+        $months = array();
         for ($month = 1; $month <= 12 ;$month++) {
-            $tableHeads[$month] = date("M", mktime(0, 0, 0, $month, 10));
+            $months[$month] = date("M", mktime(0, 0, 0, $month, 10));
         }
+        $tableHeads = array_merge(array(
+            0   => "Profile {$year}"
+        ), $months);
 
         /** @var $db Connection */
         $io = new SymfonyStyle($input,$output);
         $db = $this->getContainer()->get('database_connection');
 
-        $rawData = $db->query('
-            SELECT profiles.profile_id, profiles.profile_name, month(views.date) as month, sum(views.views) as sum_views
+        /* get profiles */
+        $profiles = $db->query('SELECT profile_id, profile_name FROM profiles')->fetchAll();
+        
+        /* get view data grouped by monts and profile ids */
+        /* TODO: possible sql injection, should escape with framework tools */
+        $rawData = $db->query("
+            SELECT profiles.profile_id, month(views.date) as month, sum(views.views) as sum_views
             FROM views
             JOIN profiles ON profiles.profile_id = views.profile_id
-            WHERE year(views.date) = 2015
+            WHERE year(views.date) = {$year}
             GROUP BY month(views.date), views.profile_id
             ORDER BY views.profile_id, month(views.date)
-        ')->fetchAll();
+        ")
+        ->fetchAll();
 
-        $tableData = array();
+        /* sort data into multidimensional array by profile id and month */
+        /* format number according the document */
+        $viewsData = array();
         foreach ($rawData as $row) {
-            if (!isset($tableData[$row['profile_id']])) {
-                $tableData[$row['profile_id']]['Profile'] = $row['profile_name'];
+            $profileId = $row['profile_id'];
+            $month = $row['month'];
+            $views = $row['sum_views'];
+
+            $viewsData[$profileId][$month] = number_format($views, 0, ".", ",");
+        }
+        
+        /* build table data based on months array, profile array and view data array */
+        $tableData = array();
+        foreach ($profiles as $profile) {
+            $profileId = $profile['profile_id'];
+            $profileName = $profile['profile_name'];
+
+            $tableData[$profileId]['profile'] = $profileName;
+            foreach ($months as $monthNumber => $monthName) {
+                $tableData[$profileId][$monthNumber] = ($viewsData[$profileId][$monthNumber]) ? 
+                $viewsData[$profileId][$monthNumber]: 
+                'n/a';
             }
-            $tableData[$row['profile_id']][$row['month']] = number_format($row['sum_views'], 0, ".", ",");
         }
 
         // Show data in a table - headers, data
