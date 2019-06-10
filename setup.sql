21f258aeec4ea61687826aa325cb46d4610d12d3
dlabs dlabs <dlabs@dlabss-MacBook-Pro.local>
2019-06-10 09:57:02 +0200

    Add primary key to tables.

--- setup.sql
+++ setup.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS `bof_test`;
 CREATE DATABASE IF NOT EXISTS `bof_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-#DROP USER IF EXISTS 'bof-test'@'localhost'; # Works only on mysql 5.7+
 DROP USER 'bof-test'@'localhost';
+
 CREATE USER 'bof-test'@'localhost' IDENTIFIED BY 'bof-test';
 
 GRANT USAGE ON * . * TO 'bof-test'@'localhost' IDENTIFIED BY 'bof-test' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
@@ -11,15 +11,16 @@ GRANT ALL PRIVILEGES ON `bof\_test` . * TO 'bof-test'@'localhost' WITH GRANT OPT
 
 DROP TABLE IF EXISTS `bof_test`.`profiles`;
 CREATE TABLE `bof_test`.`profiles` (
-`profile_id` INT NOT NULL ,
+`profile_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `profile_name` VARCHAR( 100 ) NOT NULL
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 DROP TABLE IF EXISTS `bof_test`.`views`;
 CREATE TABLE `bof_test`.`views` (
+`view_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `profile_id` INT NOT NULL ,
 `date` DATE NOT NULL ,
-`views` INT NOT NULL
+`views` INT NOT NULL 
 ) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 INSERT INTO `bof_test`.`profiles` VALUES(1, 'Karl Lagerfeld'), (2, 'Anna Wintour'), (3, 'Tom Ford'), (4, 'Pierre Alexis Dumas'), (5, 'Sandra Choi');
